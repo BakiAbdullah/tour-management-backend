812ae6d844985834e80ab51bdeb999cd18f0e3eb
Baki Abdullah <shatil605@gmail.com>
2025-07-08 23:29:24 +0600

    project setup Completed

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
+/* eslint-disable no-console */
 import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
+import { envVars } from "./app/config/env";
 
 let server: Server;
 
 const startServer = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://bakiabdullah:bakiabdullah@cluster0.uqgxsrk.mongodb.net/tour-management-system?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    await mongoose.connect(envVars.DB_URL as string);
     console.log("Connected to DB!!");
 
-    server = app.listen(5000, () => {
-      console.log("Server is listening to port 5000");
+    server = app.listen(envVars.PORT, () => {
+      console.log(`Server is listening to port ${envVars.PORT}`);
     });
   } catch (error) {
     console.log(error);
@@ -21,4 +21,62 @@ const startServer = async () => {
 
 startServer();
 
+//! Handling Signal termination
+process.on("SIGTERM", () => {
+  console.log("SIGTERM signal received. Shutting down gracefully...");
 
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+// This is for manual termination of the server using Ctrl + C
+process.on("SIGINT", () => {
+  console.log("SIGINT signal received. Shutting down gracefully...");
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+//! Handling unhandled rejection error & Promise
+process.on("unhandledRejection", (err) => {
+  console.log("unhandled Rejection detected... Server shutting down..", err);
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+//! Handling uncaught rejection error
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught Exception detected... Server shutting down..", err);
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+// unhandled rejection error
+// Promise.reject(new Error("I forgot to catch this promise!!"));
+
+// uncaught exception error
+// throw new Error("I forgot to handle this local error!!")
+
+/**
+ * unhandled rejection error
+ * uncaught rejection error
+ * signal termination system
+ */
